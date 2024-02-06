c82ac98f7799a0937f9d11f8198665f1ef99e174
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2024-02-06 21:40:09 +0530

    completed rendering the events

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ function uuid() {
             v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
+
 }
 
 todoAddButton.addEventListener("click",(e)=>{
     e.preventDefault(); //don't refresh the page
     todo = todoInput.value;
     if(todo.length > 0) todoList.push({id : uuid(), todo, isCompleted : false})
+    renderTodoList(todoList)
 })
 
 function renderTodoList(todoList){
-    showTodos.innerHTML = `<input type="checkbox"> <label class="todo">sky diving</label> <button>delete</button>`
+    console.log(todoList)
+    showTodos.innerHTML = todoList.map(todo => `<div><input type="checkbox"><label class="todo">${todo.todo} </label><button>delete</button></div>`);
 }
 
 renderTodoList(todoList)

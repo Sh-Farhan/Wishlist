481688a076a87a7b3a480812cbad33669e01ab6a
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2024-02-09 21:19:56 +0530

    final

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let todoInput = document.querySelector(".input");
 let todoAddButton = document.querySelector(".button");
 let showTodos = document.querySelector(".todos-container");
 let todo;
-let todoList = [];
+let localData =  JSON.parse(localStorage.getItem("todo"))
+let todoList = localData || [];
 
 function uuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -19,12 +20,27 @@ todoAddButton.addEventListener("click",(e)=>{
     e.preventDefault(); //don't refresh the page
     todo = todoInput.value;
     if(todo.length > 0) todoList.push({id : uuid(), todo, isCompleted : false})
+    renderTodoList(todoList);
+    localStorage.setItem("todo",JSON.stringify(todoList));
+    todoInput.value = "";
+})
+
+showTodos.addEventListener("click",(e) =>{
+    let key = e.target.dataset.key; 
+    let delTodokey = e.target.dataset.todokey;
+    todoList = todoList.map(todo=> todo.id === key ? {...todo, isCompleted : !todo.isCompleted} : todo);
+    todoList = todoList.filter(todo => todo.id !== delTodokey)
     renderTodoList(todoList)
+    localStorage.setItem("todo",JSON.stringify(todoList));
+    console.log(todoList)
 })
 
 function renderTodoList(todoList){
     console.log(todoList)
-    showTodos.innerHTML = todoList.map(todo => `<div><input type="checkbox"><label class="todo">${todo.todo} </label><button>delete</button></div>`);
+    showTodos.innerHTML = todoList.map(({id,todo,isCompleted}) => 
+    `<div class="relative"><input id="item-${id}" type="checkbox" data-key=${id} ${isCompleted ? "checked" : " "}>
+    <label for="item-${id}" class="todo todo-text t-pointer ${isCompleted ? "checked-todo" : ""}" data-key=${id}>${todo} </label>
+    <button class="" data-todokey=${id}><span data-todokey=${id}  class="del-btn material-icons-outlined">delete</span></button></div>`);
 }
 
 renderTodoList(todoList)
